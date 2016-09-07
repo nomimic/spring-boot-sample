c4eeda3b76738f7ee474ff6b5dc9ca4ff582d841
Lucas Yonghee Lee <lucas@Lucas-MacBook-Pro.local>
2016-09-07 10:33:59 +0900

    starter 초기 샘플 구성 완료

---
+++ spring-boot-starter/src/main/java/nomimic/springboot/starter/Application.java
@@ -0,0 +1,15 @@
+package nomimic.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Lucas Yonghee Lee on 2016. 9. 5..
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
