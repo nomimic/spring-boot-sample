bd94d395350b028b5482703bff3cc374ce860ada
Lucas Yonghee Lee <lucas@Lucas-MacBook-Pro.local>
2016-09-09 11:36:18 +0900

    Filter 순서 조정

--- spring-boot-starter/src/main/java/nomimic/springboot/starter/config/FilterConfig.java
+++ spring-boot-starter/src/main/java/nomimic/springboot/starter/config/FilterConfig.java
@@ -4,6 +4,7 @@ import nomimic.springboot.starter.filter.MyFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 /**
  * Created by Lucas Yonghee Lee on 2016. 9. 9..
@@ -16,6 +17,8 @@ public class FilterConfig {
         FilterRegistrationBean myFilterBean = new FilterRegistrationBean();
 
         myFilterBean.setFilter(new MyFilter());
+
+        myFilterBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return myFilterBean;
     }
